78452378a4a22fea970346c4fc7c71a749dedbe9
Andrew Janong <andrewjanong@gmail.com>
2023-08-19 23:32:33 +0800

    Fixed type and navbar styling

--- src/components/ContactMe/ContactMe.js
+++ src/components/ContactMe/ContactMe.js
@@ -9,7 +9,7 @@ const ContactMe = () => {
                 <h1>Let's Get In Touch!</h1>
                 <p>
                     I'm currently looking for a Software Engineering Internship from May to August 2024.
-                    If you believe that I would enhance your team or if you'd like to have a conversation, please don't hesitate to react out to me!
+                    If you believe that I would enhance your team or if you'd like to have a conversation, please don't hesitate to reach out to me!
                 </p>
                 <a href="mailto: andrew.janong@u.nus.edu">
                     <button className={`${styles["contact-button"]}`}>
